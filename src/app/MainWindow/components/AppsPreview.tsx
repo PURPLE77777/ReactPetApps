cfdbb75f5916973db1e46a9d631e27362a8e9a2b
PURPLE77777 <stefanovich.pasha@yandex.by>
2023-02-24 23:20:26 +0300

    some updates of apps presenting and installing react-router

--- src/app/MainWindow/components/AppsPreview.tsx
+++ src/app/MainWindow/components/AppsPreview.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../scss/appPreview.scss';
-import ProjectPreview from './ProjectPreview';
 
 interface Project {
 	id: number;
@@ -16,29 +15,36 @@ export default function AppsPreview({ projects }: AppsPreviewProps) {
 	return (
 		<div className='app-preview_container'>
 			<div className='app-preview_wrap'>
-				{!projects ? (
-					<div className='no-apps'>
-						<h4>У вас ещё нет проектов</h4>
-					</div>
-				) : (
-					// <div
-					// 	className='app'
-					// 	style={{ backgroundImage: 'url(../img/cloud.png)' }} />
-					<>
-						{projects.map(project => {
+				<>
+					{projects &&
+						projects.map(project => {
 							return (
 								<div
 									key={`app-${project.id}`}
-									className='app'
-									style={{
-										backgroundImage: project.imgPreview
-											? `url(${require('../img/' + project.imgPreview)})`
-											: `url(${require('../img/cloud.png')})`
-									}}></div>
+									className='app'>
+									<div
+										className='app_inner'
+										style={{
+											backgroundImage: project.imgPreview
+												? `url(${require('../img/' + project.imgPreview)})`
+												: `url(${require('../img/cloud.png')})`,
+											backgroundSize: project.imgPreview ? 'cover' : 'contain'
+										}}
+									/>
+								</div>
 							);
 						})}
-					</>
-				)}
+					{!projects && <h4 className='no-apps_message'>У вас ещё нет проектов</h4>}
+					<div className='app'>
+						<div
+							className='app_inner'
+							style={{
+								backgroundImage: `url(${require('../img/plus-svg.svg')})`,
+								backgroundSize: 'contain'
+							}}
+						/>
+					</div>
+				</>
 			</div>
 		</div>
 	);
