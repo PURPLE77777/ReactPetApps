7d9ec62b97845be4b6cd54e1f58818104ade5d64
PURPLE77777 <stefanovich.pasha@yandex.by>
2023-03-13 21:01:20 +0300

    updated functionality of slider

--- src/app/HDRezka/components/NewFilmsSlider.tsx
+++ src/app/HDRezka/components/NewFilmsSlider.tsx
@@ -9,7 +9,8 @@ export default function NewFilmsSlider() {
 
 	const selectChapter = (e: React.SyntheticEvent) => {
 		selectedChapter?.classList.remove('selected-chapter');
-		setSelectedChapter(e.target as HTMLSpanElement);
+		if ((e.target as HTMLSpanElement).classList.contains('show-all_span')) setSelectedChapter(null);
+		else setSelectedChapter(e.target as HTMLSpanElement);
 	};
 
 	useEffect(() => {
@@ -26,118 +27,78 @@ export default function NewFilmsSlider() {
 		sliderBtnWidth: 35,
 		sliderBtnHeight: 100,
 		countLists: 3,
-		countCardsOnOneList: 8
+		countCardsOnOneList: 8,
+		titleBottomPadding: 12,
+		titleLineHeight: 16
 	};
 	const btnSliderStyle = {
-		top: (sliderConfig.imgHeight + 8 - sliderConfig.sliderBtnHeight) / 2,
+		top:
+			(sliderConfig.imgHeight + 8 - sliderConfig.sliderBtnHeight) / 2 +
+			sliderConfig.titleBottomPadding +
+			sliderConfig.titleLineHeight,
 		height: sliderConfig.sliderBtnHeight,
 		width: sliderConfig.sliderBtnWidth
 	};
 
 	const cardsContainer: React.RefObject<HTMLDivElement> = useRef(null);
-	let currentPage = 0; // in current order by index
-	let containerPage = sliderConfig.countLists; // in slider-cards-container order by index
-	const prevFilmsPage = () => {
-		let ctr = cardsContainer.current;
-		if (ctr) {
-			if (currentPage == 0) {
-				if (containerPage == 0) {
-					ctr.style.transition = 'none';
-					ctr.style.marginLeft = `-${100 * sliderConfig.countLists + 0.55}%`;
-					let containerChilds = ctr.childNodes;
-					let mainPagesClones = [];
-					for (let i = 0; i < sliderConfig.countLists; i++) {
-						mainPagesClones.push(containerChilds[0].cloneNode(true));
-						containerChilds[0].remove();
-					}
-					for (let i = mainPagesClones.length - 1; i >= 0; i--) {
-						containerChilds[sliderConfig.countLists].before(mainPagesClones[i]);
-					}
-					containerPage = sliderConfig.countLists;
-					showLastPage();
-				} else {
-					showLastPage();
-				}
-			} else {
-				currentPage--;
-			}
-			containerPage--;
-			setTimeout(() => {
-				if (ctr) {
-					ctr.style.transition = 'margin-left 0.5s linear';
-					ctr.style.marginLeft = `calc(${ctr.style.marginLeft} + 100%)`;
-				}
-			}, 1);
-		}
-	};
-
-	const showLastPage = () => {
+	let currentPage = 0;
+	const pageWidth: number = (sliderConfig.imgWidth + 8 + 10) * sliderConfig.countCardsOnOneList;
+	const removeLastPage = () => {
 		let pages = document.querySelectorAll('.slider-page');
 		let lastPage = pages[pages.length - 1];
 		let clonePage = lastPage.cloneNode(true);
-		let prevSibl = pages[0].previousSibling;
-		let prevSiblClone = prevSibl?.cloneNode();
-		prevSibl?.remove();
+		// let prevSibl = pages[0].previousSibling;
+		// let prevSiblClone = prevSibl?.cloneNode();
+		// prevSibl?.remove();
 		pages[0].before(clonePage);
-		if (prevSiblClone) lastPage.before(prevSiblClone);
+		// if (prevSiblClone) lastPage.before(prevSiblClone);
 		lastPage.remove();
 	};
 
-	const showFirstPage = () => {
+	const removeFirstPage = () => {
 		let pages = document.querySelectorAll('.slider-page');
 		let firstPage = pages[0];
 		let clonePage = firstPage.cloneNode(true);
-		let nextSibl = pages[pages.length - 1].nextSibling;
-		let nextSiblClone = nextSibl?.cloneNode();
-		nextSibl?.remove();
+		// let nextSibl = pages[pages.length - 1].nextSibling;
+		// let nextSiblClone = nextSibl?.cloneNode();
+		// nextSibl?.remove();
 		pages[pages.length - 1].after(clonePage);
-		if (nextSiblClone) firstPage.before(nextSiblClone);
+		// if (nextSiblClone) firstPage.before(nextSiblClone);
 		firstPage.remove();
 	};
 
-	const nextFilmsPage = () => {
+	const prevPage = () => {
 		let ctr = cardsContainer.current;
-		if (ctr) {
-			if (currentPage == sliderConfig.countLists - 1) {
-				if (containerPage == sliderConfig.countLists * 3 - 1) {
-					ctr.style.marginLeft = `-${100 * (sliderConfig.countLists * 2 - 1) + 0.55}%`;
-					ctr.style.transition = 'none';
-					let containerChilds = ctr.childNodes;
-					let mainPagesClones = [];
-					for (let i = 0; i < sliderConfig.countLists; i++) {
-						mainPagesClones.push(containerChilds[containerChilds.length - 1].cloneNode(true));
-						containerChilds[containerChilds.length - 1].remove();
-					}
-					for (let i = 0; i < mainPagesClones.length; i++) {
-						containerChilds[sliderConfig.countLists - 1].after(mainPagesClones[i]);
-					}
-					containerPage = sliderConfig.countLists * 2 - 1;
-					showFirstPage();
-				} else {
-					showFirstPage();
-				}
-			} else {
-				currentPage++;
-			}
-			containerPage++;
-			setTimeout(() => {
-				if (ctr) {
-					ctr.style.transition = 'margin-left 0.5s linear';
-					ctr.style.marginLeft = `calc(${ctr.style.marginLeft} - 100%)`;
-				}
-			}, 1);
+		if (ctr && currentPage == 0) {
+			ctr.style.transition = 'none';
+			ctr.style.marginLeft = `calc(${ctr.style.marginLeft} - ${pageWidth}px)`;
+			removeLastPage();
+		} else {
+			currentPage--;
 		}
+		setTimeout(() => {
+			if (ctr) {
+				ctr.style.transition = 'margin-left 0.5s linear';
+				ctr.style.marginLeft = `calc(${ctr.style.marginLeft} + ${pageWidth}px)`;
+			}
+		}, 1);
 	};
 
-	const createSliderPageClone = (num: number) => {
-		return (
-			<div
-				style={{
-					minWidth: (sliderConfig.imgWidth + 8 + 10) * sliderConfig.countCardsOnOneList
-				}}
-				key={`prev-page-${num}`}
-			/>
-		);
+	const nextPage = () => {
+		let ctr = cardsContainer.current;
+		if (ctr && currentPage == sliderConfig.countLists - 1) {
+			ctr.style.marginLeft = `calc(${ctr.style.marginLeft} + ${pageWidth}px)`;
+			ctr.style.transition = 'none';
+			removeFirstPage();
+		} else {
+			currentPage++;
+		}
+		setTimeout(() => {
+			if (ctr) {
+				ctr.style.transition = 'margin-left 0.5s linear';
+				ctr.style.marginLeft = `calc(${ctr.style.marginLeft} - ${pageWidth}px)`;
+			}
+		}, 1);
 	};
 
 	const createSliderPage = (num: number) => {
@@ -173,7 +134,6 @@ export default function NewFilmsSlider() {
 				</div>
 				<div className='short-desc'>
 					<p className='film-title'>{film.name}</p>
-					<span style={{ fontSize: 16, color: 'red' }}>{film.id}</span>
 					<p className='film-short-info'>
 						{new Date(film.date).getFullYear()}
 						{film.country.length > 1 ? ` - ..., ` : `, `}
@@ -186,78 +146,75 @@ export default function NewFilmsSlider() {
 
 	return (
 		<div className='slider-container'>
-			<div className='chapters'>
-				Новые{' '}
-				<span
-					className='chapter'
-					onClick={selectChapter}>
-					фильмы
-				</span>
-				,{' '}
-				<span
-					className='chapter'
-					onClick={selectChapter}>
-					сериалы
-				</span>
-				,{' '}
-				<span
-					className='chapter'
-					onClick={selectChapter}>
-					мультфильмы
-				</span>{' '}
-				и{' '}
-				<span
-					className='chapter'
-					onClick={selectChapter}>
-					аниме
-				</span>
-				<span
-					className='chapter-show-all'
-					onClick={selectChapter}>
-					{' '}
-					(
-					<span
-						className='chapter-show-all_span'
-						onClick={selectChapter}>
-						отобразить все новинки
-					</span>
-					)
-				</span>
-			</div>
 			<div className='slider-btns-wrap'>
 				<button
 					className='slider-btn-prev'
 					style={btnSliderStyle}
-					onClick={prevFilmsPage}>
+					onClick={prevPage}>
 					<span className='slider-btn-prev_arrow-left' />
 				</button>
 				<button
 					className='slider-btn-next'
 					style={btnSliderStyle}
-					onClick={nextFilmsPage}>
+					onClick={nextPage}>
 					<span className='slider-btn-prev_arrow-right' />
 				</button>
 			</div>
 			<div
 				className='slider-cards-wrap'
 				style={{
-					width: (sliderConfig.imgWidth + 8 + 10) * sliderConfig.countCardsOnOneList
+					width: pageWidth - 10
 				}}>
+				<div
+					className='chapters'
+					style={{
+						paddingBottom: sliderConfig.titleBottomPadding,
+						lineHeight: `${sliderConfig.titleLineHeight}px`
+					}}>
+					Новые{' '}
+					<span
+						className='chapter'
+						onClick={selectChapter}>
+						фильмы
+					</span>
+					,{' '}
+					<span
+						className='chapter'
+						onClick={selectChapter}>
+						сериалы
+					</span>
+					,{' '}
+					<span
+						className='chapter'
+						onClick={selectChapter}>
+						мультфильмы
+					</span>{' '}
+					и{' '}
+					<span
+						className='chapter'
+						onClick={selectChapter}>
+						аниме
+					</span>
+					{selectedChapter && (
+						<>
+							{' '}
+							<small
+								className='chapter'
+								onClick={selectChapter}>
+								(<span className='show-all_span'>отобразить все новинки</span>)
+							</small>
+						</>
+					)}
+				</div>
 				<div
 					className='slider-cards-container'
 					style={{
-						marginLeft: '-300.55%',
+						marginLeft: '-10px',
 						transition: 'margin-left 0.5s linear'
 					}}
 					ref={cardsContainer}>
 					{Array.from({ length: sliderConfig.countLists }, (_, i) => i).map(num => {
-						return createSliderPageClone(10 + num);
-					})}
-					{Array.from({ length: sliderConfig.countLists }, (_, i) => i).map(num => {
 						return createSliderPage(num);
-					})}
-					{Array.from({ length: sliderConfig.countLists }, (_, i) => i).map(num => {
-						return createSliderPageClone(20 + num);
 					})}
 				</div>
 			</div>
